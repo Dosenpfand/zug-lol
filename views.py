e913b3792add33bd351ac4740a26d746df5263cb
Dosenpfand <m@sad.bz>
2022-06-10 17:39:41 +0200

    Rename ambigious form

--- views.py
+++ views.py
@@ -13,13 +13,13 @@ from app import db
 ticket_price = Blueprint('ticket_price', __name__, template_folder='templates')
 
 
-@ticket_price.route('/form', methods=['GET', 'POST'])
+@ticket_price.route('/price_form', methods=['GET', 'POST'])
 @ticket_price.route('/', methods=['GET', 'POST'])
 def price_form():
     form = PriceForm()
     if form.validate_on_submit():
         return render_template('sse_container.html', form=form)
-    return render_template('form.html', form=form)
+    return render_template('price_form.html', form=form)
 
 
 @ticket_price.route('/get_price')
