3c48c14c80f7d6f442220222f831967733dc8fa2
Dosenpfand <m@sad.bz>
2022-06-14 14:36:31 +0200

    Minor translation fix

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from flask_babel import lazy_gettext as _
+
 # secrets.token_urlsafe()
 SECRET_KEY = 'sxnyY8z4wz862Vzzq33GM3yBq09SuSZyHuvHCt9hWh0'
 # secrets.SystemRandom().getrandbits(128)
@@ -12,6 +14,8 @@ SECURITY_REGISTERABLE = True
 SECURITY_SEND_REGISTER_EMAIL = False
 SECURITY_REGISTER_USER_TEMPLATE = 'register.html'
 SECURITY_LOGIN_USER_TEMPLATE = 'login.html'
+# TODO: temporary until translation is fixed
+SECURITY_MSG_LOGIN = (_("Please log in to access this page."), "info")
 KLIMATICKET_DEFAULT_PRICE = 1095
 BABEL_DEFAULT_LOCALE = 'de'
 LANGUAGES = {'en': 'English', 'de': 'Deutsch'}
