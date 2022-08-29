da4c1a7d0852146eff6ec80d635b27d1ae108229
Dosenpfand <m@sad.bz>
2022-08-30 00:32:25 +0200

    Redesign logo and add meta tags

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from flask_babel import lazy_gettext as _
+
 SECRET_KEY = 'fffffffffffffffffffffffffffffffffffffffffff'
 SECURITY_PASSWORD_SALT = '000000000000000000000000000000000000000'
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://zug:zug@localhost:5432/zug"
@@ -14,6 +16,8 @@ KLIMATICKET_DEFAULT_PRICE = 1095
 BABEL_DEFAULT_LOCALE = 'de'
 LANGUAGES = {'en': 'English', 'de': 'Deutsch'}
 SITE_TITLE = 'zug.lol'
+SITE_DESCRIPTION = _('Search for train ticket prices in Austria and create a travel journal, to find out if your Klimaticket pays off.')
+SITE_BASE_URI = 'https://zug.lol'
 API_USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0'
 API_HOST = 'https://shop.oebbtickets.at'
 RECAPTCHA_SCRIPT = 'https://hcaptcha.com/1/api.js'
