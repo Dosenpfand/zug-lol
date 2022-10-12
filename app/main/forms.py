09569cc6d7230119371464325d2960831759ea0f
Dosenpfand <m@sad.bz>
2022-10-12 10:19:03 +0200

    Another test for pre-commit formatting

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,7 +8,7 @@ from wtforms.validators import NoneOf
 class FlexibleFloatField(FloatField):
     def process_formdata(self, valuelist):
         if valuelist:
-            valuelist[0] = valuelist[0].replace(",", '.')
+            valuelist[0] = valuelist[0].replace(",", ".")
         return super(FlexibleFloatField, self).process_formdata(valuelist)
 
 
