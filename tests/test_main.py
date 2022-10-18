44094bb68c25cc5952565d093961e5485fed56a8
Dosenpfand <m@sad.bz>
2022-10-18 12:07:27 +0200

    Add add and delete journey tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,28 @@
+import os
+import re
+
 import pytest
 from flask_security import hash_password
 
 from app import create_app, init_db, security
+
+TEST_EMAIL = "test@example1.com"
+TEST_PASSWORD = "password"
 
 
 class AuthActions(object):
     def __init__(self, client):
         self._client = client
 
-    def login(self, email="test@example1.com", password="test1234"):
-        return self._client.post(
-            "/login", data={"email": email, "password": password, "submit": "Login"}
+    def login(self, email=TEST_EMAIL, password=TEST_PASSWORD):
+        response = self._client.post(
+            "/login",
+            data=dict(email=email, password=password, submit="Login"),
+            follow_redirects=True,
         )
+        assert response.status_code == 200
+        assert response.request.path == "/"
+        return response
 
     def logout(self):
         return self._client.get("/logout")
@@ -32,14 +43,13 @@ def app():
     with app.app_context():
         init_db(drop=False)
 
-        if not security.datastore.find_user(email="test@example1.com"):
+        if not security.datastore.find_user(email=TEST_EMAIL):
             security.datastore.create_user(
-                email="test@example1.com", password=hash_password("password"), roles=[]
+                email=TEST_EMAIL, password=hash_password(TEST_PASSWORD), roles=[]
             )
         security.datastore.db.session.commit()
 
     yield app
-    # TODO: clean up
 
 
 @pytest.fixture()
@@ -52,13 +62,83 @@ def client(app):
     return app.test_client()
 
 
-def test_request_price_form(client):
-    response = client.get("/price_form")
-    assert b"<form" in response.data
+# TODO
+# ticket price search
+# determining price for journey
+# exporting journeys
+# importing journeys
+# deleting all journeys
+# delete account
 
 
-def test_login_wrong_credentials(auth, client):
-    assert client.get("/login").status_code == 200
-    response = auth.login()
-    assert response.status_code == 200
-    assert "Ungültiges Passwort" in response.text
+class TestJourneys:
+    @staticmethod
+    def row_pattern(origin, destination, price):
+        return (
+            rf"<td>{origin}</td>(\s*)<td>{destination}</td>(\s*)<td>{price}</td>(\s*)"
+            r'<td>(.*?)</td>(\s*)<td>(\s*)<form(.*?)action="/delete_journey/(?P<id>\d+)"'
+        )
+
+    journeys = [
+        ("Wien", "Innsbruck", "99"),
+        ("Bozen", "Wels", "77"),
+        ("St. Pölten", "Melk", "10"),
+        ("Linz", "Berlin", "42"),
+    ]
+
+    @pytest.mark.parametrize("origin,destination,price", journeys)
+    def test_add_journey(self, auth, client, origin, destination, price):
+        auth.login()
+        response = client.get("/journeys")
+        assert not re.search(
+            self.row_pattern(origin, destination, price),
+            response.text,
+            flags=re.MULTILINE | re.DOTALL,
+        )
+
+        response = client.post(
+            "/journeys",
+            data=dict(
+                origin=origin,
+                destination=destination,
+                price=price,
+                date="",
+                submit="Add+Journey",
+            ),
+            follow_redirects=True,
+        )
+
+        assert response.status_code == 200
+        assert re.search(
+            self.row_pattern(origin, destination, price),
+            response.text,
+            flags=re.MULTILINE | re.DOTALL,
+        )
+
+    @pytest.mark.parametrize("origin,destination,price", journeys)
+    def test_delete_journey(self, auth, client, origin, destination, price):
+        auth.login()
+        response = client.get("/journeys")
+        assert response.status_code == 200
+        match = re.search(
+            self.row_pattern(origin, destination, price),
+            response.text,
+            flags=re.MULTILINE | re.DOTALL,
+        )
+        journey_id = match.group("id")
+
+        response = client.post(
+            f"/delete_journey/{journey_id}",
+            follow_redirects=True,
+        )
+        assert response.status_code == 200
+        assert not re.search(
+            self.row_pattern(origin, destination, price),
+            response.text,
+            flags=re.MULTILINE | re.DOTALL,
+        )
+
+    # TODO: Teardown (and whole class) should not be necessary, but in memory db results in error
+    @staticmethod
+    def teardown_class():
+        os.remove("app/app.db")
