39580a31bf21527843d5fff9bcef98a5b4bccc40
Dosenpfand <m@sad.bz>
2023-03-05 14:30:46 +0100

    Add missing type hints

--- app/error/views.py
+++ app/error/views.py
@@ -1,11 +1,12 @@
+from typing import Tuple
+
 from flask import render_template
 from flask_babel import gettext as _
 
 
-def page_not_found(e):
+def page_not_found(e: Exception) -> Tuple[str, int]:
     return render_template("404.html", title=_("Page Not Found")), 404
 
 
-def internal_server_error(e):
-    # raise e
+def internal_server_error(e: Exception) -> Tuple[str, int]:
     return render_template("500.html", title=_("Internal Server Error")), 500
