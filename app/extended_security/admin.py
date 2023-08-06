5783c83b7c19835fb33369f04fb5d1ad016c47cd
Dosenpfand <m@sad.bz>
2023-08-06 16:36:40 +0200

    Fix types

--- app/extended_security/admin.py
+++ app/extended_security/admin.py
@@ -1,3 +1,4 @@
+import typing
 from flask import abort, current_app
 from flask_admin.form import SecureForm
 from flask_admin.contrib.sqla import ModelView
@@ -5,20 +6,16 @@ from flask_admin.base import AdminIndexView
 from flask_security import current_user
 
 
-def user_formatter(view, context, model, name):
-    return f"{model.user.email}"
-
-
 class AdminSecurityMixIn:
-    def is_accessible(self):
+    def is_accessible(self) -> bool:
         return (
             current_user.is_active
             and current_user.is_authenticated
             and current_user.has_role(current_app.config["ADMIN_ROLE_NAME"])
         )
 
-    def inaccessible_callback(self, name, **kwargs):
-        return abort(404)
+    def inaccessible_callback(self, name: str, **kwargs: typing.Any) -> typing.NoReturn:
+        abort(404)
 
 
 class AdminModelView(AdminSecurityMixIn, ModelView):
@@ -27,7 +24,6 @@ class AdminModelView(AdminSecurityMixIn, ModelView):
     can_view_details = True
     can_export = True
     form_base_class = SecureForm
-    column_formatters = dict(user=user_formatter)
 
 
 class ExtendedAdminIndex(AdminSecurityMixIn, AdminIndexView):
