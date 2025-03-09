b1db39b6a6adec61c5c6c7a7c62ba9c351e65f35
Dosenpfand <git@sad.bz>
2025-03-09 14:21:33 +0100

    Add sentry to cron

--- docker/cron.sh
+++ docker/cron.sh
@@ -2,6 +2,21 @@
 
 set -eu -o pipefail
 
+trap 'cleanup' ERR
+cleanup() {
+    if [ -n "${SENTRY_CRONS}" ]; then
+        curl "${SENTRY_CRONS}?status=error"
+    fi
+}
+
+if [ -n "${SENTRY_CRONS}" ]; then
+    curl "${SENTRY_CRONS}?status=in_progress"
+fi
+
 . /root/get_env.sh
 cd /app
 flask update-oldest-price 10 30 >> /var/log/cron.log 2>&1
+
+if [ -n "${SENTRY_CRONS}" ]; then
+    curl "${SENTRY_CRONS}?status=ok"
+fi
