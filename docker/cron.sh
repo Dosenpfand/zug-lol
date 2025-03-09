fd894e68b6c6f81d74a80aca74cc9a61dc8f6436
Dosenpfand <git@sad.bz>
2025-03-09 14:44:36 +0100

    Fix reporting

--- docker/cron.sh
+++ docker/cron.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 set -eu -o pipefail
+. /root/get_env.sh
 
 trap 'cleanup' ERR
 cleanup() {
-    if [ -n "${SENTRY_CRONS}" ]; then
+    if [ -n "${SENTRY_CRONS:-}" ]; then
         curl "${SENTRY_CRONS}?status=error"
     fi
 }
 
-if [ -n "${SENTRY_CRONS}" ]; then
+if [ -n "${SENTRY_CRONS:-}" ]; then
     curl "${SENTRY_CRONS}?status=in_progress"
 fi
 
-. /root/get_env.sh
 cd /app
 flask update-oldest-price 10 30 >> /var/log/cron.log 2>&1
 
-if [ -n "${SENTRY_CRONS}" ]; then
+if [ -n "${SENTRY_CRONS:-}" ]; then
     curl "${SENTRY_CRONS}?status=ok"
 fi
